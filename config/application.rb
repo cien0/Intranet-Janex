0e52fe55389e893f5ab194368f18974acac2cdba
cien0 <marekolszewski@marekolszewski.pl>
2016-08-12 15:08:07 +0200

    Poprawki 12/08/2016

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,15 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.i18n.default_locale = :pl
 
 module Intranet
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+I18n.locale = :"pl"
+config.i18n.default_locale = :pl
+
   end
 end
